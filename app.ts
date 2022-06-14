6479df410c802021f511de58bf1114d425cba953
Michael Lindstrom <michael.lindstrom@juvare.com>
2022-06-14 15:19:05 -0500

    try cwd with cli opine and eta example

--- app.ts
+++ app.ts
@@ -21,7 +21,8 @@ const app = opine();
 
 // View engine setup
 app.engine("eta", renderFile);
-app.set("views", join(__dirname, "views"));
+//app.set("views", join(__dirname, "views"));
+app.set("views", `${Deno.cwd()}/views`);
 app.set("view engine", "eta");
 
 // Handle different incoming body types
