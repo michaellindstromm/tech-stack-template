d70f6afd0c1e462c6d4cb7322c70209db644a9cd
Michael Lindstrom <michael.lindstrom@juvare.com>
2022-06-14 08:16:51 -0500

    use cwd instead of std dir and join

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 import { opine, serveStatic } from "https://deno.land/x/opine@2.2.0/mod.ts";
 import { config } from "https://deno.land/std@0.143.0/dotenv/mod.ts";
 import { renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts"
-import { dirname, join } from "https://deno.land/std@0.143.0/path/mod.ts";
-
 import router from "./src/router.ts";
 
 await config({ safe: true })
@@ -11,11 +9,10 @@ const ENV = Deno.env.get("ENV") || 'DEV'
 const PRODUCTION = ENV === 'production'
 
 const app = opine()
-const __dirname = dirname(import.meta.url)
 
 app.engine(".html", renderFile)
-app.set("views", join(__dirname, "views"))
-app.use(serveStatic(join(__dirname, "public")))
+app.set("views", `${Deno.cwd()}/views`)
+app.use(serveStatic(`${Deno.cwd()}/public`))
 app.set("view engine", "html")
 app.set("view cache", PRODUCTION);
 app.use('/', router)
