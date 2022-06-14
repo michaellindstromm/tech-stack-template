4a2c3b0836ea1463906e1bb78830f60f8d0792ad
Michael Lindstrom <michael.lindstrom@juvare.com>
2022-06-14 10:34:16 -0500

    okay.... changing back to cwd with logging

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ import { opine, serveStatic } from "https://deno.land/x/opine@2.2.0/mod.ts";
 import { config } from "https://deno.land/std@0.143.0/dotenv/mod.ts";
 import { renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts"
 import router from "./lib/router.ts";
-import { dirname, join } from "https://deno.land/std@0.143.0/path/mod.ts";
 
 await config({ safe: true })
 const PORT = Deno.env.get("PORT") || '8000'
@@ -10,9 +9,8 @@ const ENV = Deno.env.get("ENV") || 'DEV'
 const PRODUCTION = ENV === 'production'
 
 const app = opine()
-const __dirname = dirname(import.meta.url);
 app.engine(".html", renderFile);
-const viewsPath = join(__dirname, "views")
+const viewsPath = `${Deno.cwd()}/views`
 console.log('viewsPath: ', viewsPath)
 const fileNames: string[] = [];
   
@@ -24,9 +22,9 @@ for await (const dirEntry of Deno.readDir(`${Deno.cwd()}/views`)) {
 
 console.log(fileNames);
 app.set("views", viewsPath);
-app.use(serveStatic(join(__dirname, "public")));
+app.use(serveStatic(`${Deno.cwd()}/views`));
 app.set("view engine", "html")
-app.set("view cache", false);
+app.set("view cache", PRODUCTION);
 app.use('/', router)
 
 if (import.meta.main) {
