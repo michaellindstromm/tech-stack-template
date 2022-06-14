9017fc6a62cde937de17085b1e3b13e67c621f8e
Michael Lindstrom <michael.lindstrom@juvare.com>
2022-06-14 15:10:54 -0500

    okay let's add some console logs

--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,16 @@ import { Router } from "../deps.ts";
 const router = Router();
 
 // GET home page.
-router.get("/", (_req, res, _next) => {
+router.get("/", (req, res, _next) => {
+  console.log('app: ', req.app)
+  console.log('View: ', req.app.get('view'))
+  const View = req.app.get("view");
+  const view = new View('index', {
+    defaultEngine: req.app.get("view engine"),
+    engines: req.app.engines,
+    root: req.app.get("views"),
+  });
+  console.log('view: ', view)
   res.render("index", {
     title: "Opine",
   });
