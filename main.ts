a0a5ad6a0269a329ddd3aee882389eb7447a9054
Michael Lindstrom <michael.lindstrom@juvare.com>
2022-06-14 09:06:40 -0500

    switched to using dirname and join from std instead of Deno.cwd

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { opine, serveStatic } from "https://deno.land/x/opine@2.2.0/mod.ts";
 import { config } from "https://deno.land/std@0.143.0/dotenv/mod.ts";
 import { renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts"
 import router from "./lib/router.ts";
+import { dirname, join } from "https://deno.land/std@0.143.0/path/mod.ts";
 
 await config({ safe: true })
 const PORT = Deno.env.get("PORT") || '8000'
@@ -9,10 +10,10 @@ const ENV = Deno.env.get("ENV") || 'DEV'
 const PRODUCTION = ENV === 'production'
 
 const app = opine()
-
-app.engine(".html", renderFile)
-app.set("views", `${Deno.cwd()}/views`)
-app.use(serveStatic(`${Deno.cwd()}/public`))
+const __dirname = dirname(import.meta.url);
+app.engine(".html", renderFile);
+app.set("views", join(__dirname, "views"));
+app.use(serveStatic(join(__dirname, "public")));
 app.set("view engine", "html")
 app.set("view cache", PRODUCTION);
 app.use('/', router)
